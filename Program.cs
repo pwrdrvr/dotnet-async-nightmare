fb1deeca5589b0b183d572d73d16ce0276d68545
Harold Hunt <harold@pwrdrvr.com>
2025-03-16 17:36:02 -0400

    Listen on all IPs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿AdjustThreadPool();
 
 var builder = WebApplication.CreateBuilder();
-builder.WebHost.ConfigureKestrel(o => o.AddServerHeader = false);
+builder.WebHost.ConfigureKestrel(o => { o.AddServerHeader = false; o.ListenAnyIP(5000); });
 builder.Logging.ClearProviders();
 
 var app = builder.Build();
